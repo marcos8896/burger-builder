a3ae71af769133309b38570d1e651f63532862b0
marcos8896 <elyomarcos@gmail.com>
2018-05-17 17:31:47 -0500

    Section 8 - Lecture 120 - Outputting Multiple Build Controls.

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -7,8 +7,8 @@ const buildControl = (props) => {
   return (
     <div className={classes.BuildControl}>
       <div className={classes.Label}>{props.label}</div>
-      <button>Less</button>
-      <button>More</button>
+      <button className={classes.Less}>Less</button>
+      <button className={classes.More}>More</button>
     </div>
   )
 }
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,16 +1,27 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
 
-const buidlControls = (props) => {
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
+];
+
+const buildControls = (props) => {
   return (
-    <div>
-      
+    <div className={classes.BuildControls}>
+      {controls.map(ctrl => (
+        <BuildControl key={ctrl.label} label={ctrl.label}/> 
+      ))}
     </div>
   )
 }
 
-buidlControls.propTypes = {
+buildControls.propTypes = {
 
 }
 
-export default buidlControls
+export default buildControls
